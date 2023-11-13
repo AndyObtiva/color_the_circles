85ac25aa1ca5064890db305713c6094fcebafb1c
Andy Maleh <andy.am@gmail.com>
2023-11-13 15:17:24 -0800

    Darken general color range for circles to make it more possible to see them on top of a white canvas

--- app/color_the_circles/view/color_the_circles.rb
+++ app/color_the_circles/view/color_the_circles.rb
@@ -7,6 +7,7 @@ class ColorTheCircles
     
       WINDOW_WIDTH = 800
       WINDOW_HEIGHT = 600
+      COLOR_RANGE = (0..200)
       SHAPE_MIN_SIZE = 15
       SHAPE_MAX_SIZE = 75
       MARGIN_WIDTH = 55
@@ -190,7 +191,7 @@ class ColorTheCircles
         circle_x = rand * (WINDOW_WIDTH - MARGIN_WIDTH - SHAPE_MAX_SIZE) + SHAPE_MAX_SIZE
         circle_y = rand * (WINDOW_HEIGHT - MARGIN_HEIGHT - SHAPE_MAX_SIZE) + SHAPE_MAX_SIZE
         circle_size = rand * (SHAPE_MAX_SIZE - SHAPE_MIN_SIZE) + SHAPE_MIN_SIZE
-        stroke_color = Glimmer::LibUI.x11_colors.sample
+        stroke_color = [rand(COLOR_RANGE), rand(COLOR_RANGE), rand(COLOR_RANGE)]
         @circles_data << {
           args: [circle_x, circle_y, circle_size],
           fill: nil,
