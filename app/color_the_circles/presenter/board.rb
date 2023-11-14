1558b84d36a963e45481f1309983116fbded94b3
Andy Maleh <andy.am@gmail.com>
2023-11-13 16:18:04 -0800

    Refactor extract push_colored_circle_behind_uncolored_circles to Presenter::Board

--- app/color_the_circles/presenter/board.rb
+++ app/color_the_circles/presenter/board.rb
@@ -20,11 +20,18 @@ class ColorTheCircles
         circle_y = rand * (WINDOW_HEIGHT - MARGIN_HEIGHT - SHAPE_MAX_SIZE) + SHAPE_MAX_SIZE
         circle_size = rand * (SHAPE_MAX_SIZE - SHAPE_MIN_SIZE) + SHAPE_MIN_SIZE
         stroke_color = [rand(COLOR_RANGE), rand(COLOR_RANGE), rand(COLOR_RANGE)]
-        @circles_data << {
+        circles_data << {
           args: [circle_x, circle_y, circle_size],
           fill: nil,
           stroke: stroke_color
         }
+      end
+      
+      def push_colored_circle_behind_uncolored_circles(colored_circle_data)
+        removed_colored_circle_data = circles_data.delete(colored_circle_data)
+        last_colored_circle_data = circles_data.select {|cd| cd[:fill]}.last
+        last_colored_circle_data_index = circles_data.index(last_colored_circle_data) || -1
+        circles_data.insert(last_colored_circle_data_index + 1, removed_colored_circle_data)
       end
     end
   end
